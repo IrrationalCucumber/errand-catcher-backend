e36c8796fe5f4b01695678a0603d0996cd5b7a5d
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-12-31 15:38:32 +0800

    test config

--- functions/errand-routes.js
+++ functions/errand-routes.js
@@ -3,6 +3,6 @@ const serverless = require("serverless-http");
 const ErrandRoutes = require("../Route/ErrandRoutes");
 const app = express();
 
-app.use("/.netlify/functions/errand-routes", ErrandRoutes);
+app.use("/.netlify/functions/errand-routes/", ErrandRoutes);
 
 module.exports.handler = serverless(app);
