8fda79db851a1104b27afa2cc2298b36c5268399
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-12-31 15:56:59 +0800

    test config

--- functions/user-route.js
+++ functions/user-route.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const UserRoutes = require("../Route/UserRoutes");
+const ErrandRoutes = require("../Route/ErrandRoutes");
 const app = express();
 
-app.use("/.netlify/functions/user-routes/", UserRoutes);
+app.use(express.json()); // Ensure JSON parsing middleware is added
+app.use("/.netlify/functions/errand-routes/", ErrandRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
 
 module.exports.handler = serverless(app);
