3bc9ff217d700990259255ed0cfa4154cea2bc44
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-12-31 17:40:36 +0800

    u

--- functions/db-connection.js
+++ functions/db-connection.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: 3306,
-  connectTimeout: 10000, // Increase timeout to 10 seconds
+  connectTimeout: 26000, // Increase timeout to 10 seconds
 });
 
 db.connect((err) => {
